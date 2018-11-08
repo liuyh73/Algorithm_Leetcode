f1c16407442e565b7ad65d4b9403d9fe850a4798
liuyh73 <15989067460@163.com>
2018-11-08 22:00:04 +0800

    update 115

--- week10/leetcode--115/115. Distinct Subsequences方法2.go
+++ week10/leetcode--115/115. Distinct Subsequences方法2.go
@@ -1,15 +1,14 @@
 func numDistinct(s string, t string) int {
-    ls := len(s)
-    lt := len(t)
-    dp := make([]int, lt + 1)
+    lenS := len(s)
+    lenT := len(t)
+    dp := make([]int, lenT+ 1)
     dp[0] = 1
-    
-    for i := 1; i <= ls; i++ {
-        for j := lt; j >= 1; j-- {
+    for i := 1; i <= lenS ; i++ {
+        for j := lenT ; j >= 1; j-- {
             if s[i - 1] == t[j - 1] {
                 dp[j] += dp[j-1]
             }
         }
     }
-    return dp[lt]
+    return dp[lenT]
 }
